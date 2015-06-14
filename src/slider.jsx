0bc3522b0cc9508fea672a4a72347eee3dde8395
Ramana Venkata <idlike2dream@gmail.com>
2015-06-14 20:57:12 +0530

    :bug: fix function name

--- src/slider.jsx
+++ src/slider.jsx
@@ -44,7 +44,7 @@ var Slider = React.createClass({
     var newProps;
     if (this.state.breakpoint) {
       newProps = this.props.responsive.filter(resp => resp.breakpoint === this.state.breakpoint);
-      settings = newProps[0].settings === 'unslick' ? 'unslick' : _assign({}, this.props, newProps[0].settings);
+      settings = newProps[0].settings === 'unslick' ? 'unslick' : assign({}, this.props, newProps[0].settings);
     } else {
       settings = assign({}, defaultProps, this.props);
     }
