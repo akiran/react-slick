e51ee9e33050d49da794c9c1580314f5ca4747ab
kiran <kiran@webrafter.com>
2015-08-20 23:22:05 +0530

    version update

--- dist/react-slick.js
+++ dist/react-slick.js
@@ -322,6 +322,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	      window.clearTimeout(this.state.autoPlayTimer);
 	    }
 	  },
+	  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+	    this.update(nextProps);
+	  },
 	  componentDidUpdate: function componentDidUpdate() {
 	    this.adaptHeight();
 	  },
@@ -778,6 +781,31 @@ return /******/ (function(modules) { // webpackBootstrap
 	      this.setState({ trackStyle: trackStyle });
 
 	      this.autoPlay(); // once we're set up, trigger the initial autoplay.
+	    });
+	  },
+	  update: function update(props) {
+	    // This method has mostly same code as initialize method.
+	    // Refactor it
+	    var slideCount = _react2['default'].Children.count(props.children);
+	    var listWidth = this.refs.list.getDOMNode().getBoundingClientRect().width;
+	    var trackWidth = this.refs.track.getDOMNode().getBoundingClientRect().width;
+	    var slideWidth = this.getDOMNode().getBoundingClientRect().width / props.slidesToShow;
+
+	    this.setState({
+	      slideCount: slideCount,
+	      slideWidth: slideWidth,
+	      listWidth: listWidth,
+	      trackWidth: trackWidth
+	    }, function () {
+
+	      var targetLeft = (0, _trackHelper.getTrackLeft)((0, _objectAssign2['default'])({
+	        slideIndex: this.state.currentSlide,
+	        trackRef: this.refs.track
+	      }, props, this.state));
+	      // getCSS function needs previously set state
+	      var trackStyle = (0, _trackHelper.getTrackCSS)((0, _objectAssign2['default'])({ left: targetLeft }, props, this.state));
+
+	      this.setState({ trackStyle: trackStyle });
 	    });
 	  },
 	  adaptHeight: function adaptHeight() {
