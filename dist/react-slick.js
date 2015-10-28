ce7fcabbb2833bdff9130e9fc64244bcc237bb12
Andrew Lee <andrewlee@CACPWEB-ANDREWL.local>
2015-10-28 10:10:50 -0700

    prepublish

--- dist/react-slick.js
+++ dist/react-slick.js
@@ -253,8 +253,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 	  },
 	  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-	    if (this.props.initialSlide != nextProps.initialSlide) {
-	      this.setState({ currentSlide: nextProps.initialSlide });
+	    if (this.props.slickGoTo != nextProps.slickGoTo) {
+	      this.setState({ currentSlide: nextProps.slickGoTo });
 	    }
 	    this.update(nextProps);
 	  },
