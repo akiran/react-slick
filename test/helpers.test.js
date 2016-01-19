61a6355cc5b041fa4a2d7125ce7a7717007578e8
kiran <kiran@webrafter.com>
2016-01-19 17:59:54 +0530

    fixed test issues

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -2,20 +2,20 @@ var should = require('should');
 var sinon = require('sinon');
 var assign = require('object-assign');
 var deepMerge = require('deepmerge');
-var helpers = require('../lib/mixins/helpers');
-var initialState = require('../lib/initial-state');
-var defaultProps = require('../lib/default-props');
+var helpers = require('../src/mixins/helpers');
+var initialState = require('../src/initial-state');
+var defaultProps = require('../src/default-props');
 
 // describe('helpers.getDotCount', function () {
 //   it('dot count with slidesToShow = 1', function () {
-//     var hlepers = require('../lib/mixins/helpers');
+//     var hlepers = require('../src/mixins/helpers');
 //     helpers.state = assign(initialState, { slideCount: 6 });
 //     helpers.props = assign(defaultProps, {slidesToScroll: 1});
 //     helpers.getDotCount().should.equal(5);
 //   });
   
 //   it('dot count with slidesToShow = 4', function () {
-//     var hlepers = require('../lib/mixins/helpers');
+//     var hlepers = require('../src/mixins/helpers');
 //     helpers.state = assign(initialState, { slideCount: 16 });
 //     helpers.props = assign(defaultProps, {slidesToScroll: 4});
 //     helpers.getDotCount().should.equal(3);
@@ -24,7 +24,7 @@ var defaultProps = require('../lib/default-props');
 
 // describe('helpers.getLeft', function () {
 //   it('return left with slidesToShow = 1 ', function () {
-//     var hlepers = require('../lib/mixins/helpers');
+//     var hlepers = require('../src/mixins/helpers');
 //     helpers.state = assign(initialState, { slideCount: 6, slideWidth: 100 });
 //     helpers.props = assign(defaultProps);
 //     helpers.getLeft(0).should.equal(-100);
@@ -32,7 +32,7 @@ var defaultProps = require('../lib/default-props');
 //   });
 
 //   it('return left for slidesToShow = 4', function () {
-//     var hlepers = require('../lib/mixins/helpers');
+//     var hlepers = require('../src/mixins/helpers');
 //     helpers.state = assign(initialState, { slideCount: 6, slideWidth: 100 });
 //     helpers.props = assign(defaultProps, {slidesToShow: 4});
 //     helpers.getLeft(0).should.equal(-400);
