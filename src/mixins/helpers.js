9fce757ea7461baa4e86ba30d22058edd55fd9d3
Dominic <dominic.cocchiarella@gmail.com>
2016-02-02 13:38:28 -0800

    add option to debounce resizing functions

--- src/mixins/helpers.js
+++ src/mixins/helpers.js
@@ -80,7 +80,7 @@ var helpers = {
     var callback;
 
     if (this.props.waitForAnimate && this.state.animating) {
-	return;
+      return;
     }
 
     if (this.state.currentSlide === index) {
@@ -270,6 +270,30 @@ var helpers = {
   pause: function () {
     if (this.state.autoPlayTimer) {
       window.clearTimeout(this.state.autoPlayTimer);
+    }
+  },
+  debounce: function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this;
+      var args = arguments;
+
+      var later = function() {
+          timeout = null;
+          if ( !immediate ) {
+            func.apply(context, args);
+          }
+      };
+
+      var callNow = immediate && !timeout;
+
+      clearTimeout(timeout);
+      
+      timeout = setTimeout(later, wait || 200);
+      
+      if ( callNow ) { 
+        func.apply(context, args);
+      }
     }
   }
 };
