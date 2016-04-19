ae00be58b3deb723af75b82ca3daf0a3f1832ad9
Chris Knepper <chris82thekid@gmail.com>
2016-04-19 19:43:30 -0400

    -Correct unreliable logic in componentWillReceiveProps function in src/inner-slider.jsx (fixes #297)

--- src/inner-slider.jsx
+++ src/inner-slider.jsx
@@ -60,11 +60,12 @@ export var InnerSlider = React.createClass({
     }
   },
   componentWillReceiveProps: function(nextProps) {
-    if (this.props.slickGoTo != nextProps.slickGoTo) {
+    if (this.props.slickGoTo !== nextProps.slickGoTo
+      || this.props.children[0].key !== nextProps.children[0].key) {
       this.changeSlide({
-          message: 'index',
-          index: nextProps.slickGoTo,
-          currentSlide: this.state.currentSlide
+        message: 'index',
+        index: nextProps.slickGoTo,
+        currentSlide: this.state.currentSlide
       });
     } else {
       this.update(nextProps);
