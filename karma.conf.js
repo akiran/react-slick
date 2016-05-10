59524cc9a2dd69876c8b7221e907c329e3b74206
kiran <kiran@webrafter.com>
2016-05-10 18:31:18 +0530

    Basic test setup with enzyme

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,11 @@
 // Karma configuration
 'use strict';
-
+var path = require('path');
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
       'node_modules/es5-shim/es5-shim.js',
       'testlib/sinon.js',
@@ -22,23 +14,27 @@ module.exports = function(config) {
       'node_modules/should/should.js',
       'test/**/*.js',
     ],
-
-
-    // list of files to exclude
     exclude: [
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       './test/**/*.js': ['webpack'],
     },
 
     webpack: {
+      devtool: 'inline-source-map',
       module: {
         loaders: [
-            { loader: 'babel' },
+            { 
+              loader: 'babel', 
+              exclude: path.resolve(__dirname, 'node_modules'),
+              query: {
+                presets: ['airbnb']
+              }
+            },
+            {
+              test: /\.json$/,
+              loader: 'json',
+            },
         ]
       },
       resolve : {
@@ -57,14 +53,10 @@ module.exports = function(config) {
     },
     webpackServer: {
       // hot: true,
-      quiet: true,
+      // quiet: true,
       noInfo: true,
     },
-    // webpackPort: 8080, // Defaults to config.port + 1
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
     // coverageReporter: {
     //   type : 'html',
@@ -72,33 +64,16 @@ module.exports = function(config) {
     // },
     reportSlowerThan: 20,
 
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_WARN,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    plugins: [
+      'karma-webpack',
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
+    ],
     singleRun: false,
-    // plugins: [
-    //   require('karma-webpack')
-    // ]
   });
 };
