a85d48be776cb3dc68cb715388be54f00f01cc2c
Joshua Efiong <joshefiong@gmail.com>
2016-08-21 22:51:57 +0100

    Wrote the SwipeToSlide demo component again.

--- docs/demos.jsx
+++ docs/demos.jsx
@@ -20,6 +20,7 @@ import Fade from '../examples/Fade'
 import SlickGoTo from '../examples/SlickGoTo'
 import CustomArrows from '../examples/CustomArrows'
 import DynamicSlides  from '../examples/DynamicSlides'
+import SwipeToSlide from '../examples/SwipeToSlide'
 
 export default class App extends React.Component {
   render() {
@@ -42,6 +43,7 @@ export default class App extends React.Component {
         <SlickGoTo />
         <CustomArrows />
         <DynamicSlides />
+        <SwipeToSlide />
       </div>
     );
   }
