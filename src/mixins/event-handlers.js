458ad727f5eb4c6461a56903ea965ee0d2546ea1
Joshua Efiong <joshefiong@gmail.com>
2016-09-07 21:54:36 +0100

    Implemented getNavigableIndexes method.

--- src/mixins/event-handlers.js
+++ src/mixins/event-handlers.js
@@ -153,6 +153,30 @@ var EventHandlers = {
       e.preventDefault();
     }
   },
+  getNavigableIndexes() {
+    let max;
+    let breakPoint = 0;
+    let counter = 0;
+    let indexes = [];
+
+    if (!this.props.infinite) {
+      max = this.state.slideCount;
+    } else {
+      breakPoint = this.props.slidesToShow * -1;
+      counter = this.props.slidesToShow * -1;
+      max = this.state.slideCount * 2;
+    }
+
+    while (breakPoint < max) {
+      indexes.push(breakPoint);
+      breakPoint = counter + this.props.slidesToScroll;
+
+      counter += this.props.slidesToScroll <= this.props.slidesToShow ?
+        this.props.slidesToScroll : this.props.slidesToShow;
+    }
+
+    return indexes;
+  },
   swipeEnd: function (e) {
     if (!this.state.dragging) {
       e.preventDefault();
