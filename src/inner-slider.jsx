ea521dd7db76694af15f75d19a67dab80515f037
Asgator <apvnsk@gmail.com>
2016-09-28 17:14:36 +0700

    good lazyLoaded

--- src/inner-slider.jsx
+++ src/inner-slider.jsx
@@ -37,16 +37,18 @@ export var InnerSlider = React.createClass({
     this.setState({
       mounted: true
     });
-    var lazyLoadedList = [];
-    for (var i = 0; i < React.Children.count(this.props.children); i++) {
-      if (i >= this.state.currentSlide && i < this.state.currentSlide + this.props.slidesToShow) {
-        lazyLoadedList.push(i);
-      }
-    }
 
-    if (this.props.lazyLoad && this.state.lazyLoadedList.length === 0) {
+    if (this.props.lazyLoad) {
+      var lazyLoadedList = [];
+
+      for (var i = 0; i < React.Children.count(this.props.children); i++) {
+        if (i >= this.state.currentSlide && i < this.state.currentSlide + this.props.slidesToShow) {
+          lazyLoadedList.push(i);
+        }
+      }
+
       this.setState({
-        lazyLoadedList: lazyLoadedList
+        lazyLoadedList: this.getLazyLoadedList(this.state.currentSlide, lazyLoadedList)
       });
     }
   },
@@ -79,6 +81,13 @@ export var InnerSlider = React.createClass({
     }
   },
   componentWillReceiveProps: function(nextProps) {
+    // lazyLoad
+    if (nextProps.lazyLoad && nextProps.preLoad != this.props.preLoad) {
+      this.setState({
+        lazyLoadedList: this.getLazyLoadedList(this.state.currentSlide, null, nextProps)
+      });
+    }
+
     if (this.props.slickGoTo != nextProps.slickGoTo) {
       if (process.env.NODE_ENV !== 'production') {
         console.warn('react-slick deprecation warning: slickGoTo prop is deprecated and it will be removed in next release. Use slickGoTo method instead')
