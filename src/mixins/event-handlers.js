cdd35c3eda41190b2d3293f3d79b825367d753e2
silicakes <Mike.L.Katz@gmail.com>
2017-04-30 11:44:41 +0300

    Fixed out of bounds exception
    When the slides pulled 'outside' the screen - an exception occurs. This fixes it.

--- src/mixins/event-handlers.js
+++ src/mixins/event-handlers.js
@@ -205,9 +205,8 @@ var EventHandlers = {
 
     if (this.props.swipeToSlide) {
       let swipedSlide;
-
+      let slidesTraversed;
       const slickList = ReactDOM.findDOMNode(this.list);
-
       const slides = slickList.querySelectorAll('.slick-slide');
 
       Array.from(slides).every((slide) => {
@@ -225,8 +224,12 @@ var EventHandlers = {
 
         return true;
       });
-
-      const slidesTraversed = Math.abs(swipedSlide.dataset.index - this.state.currentSlide) || 1;
+        
+        if(swipedSlide && swipedSlide.dataset) {
+            slidesTraversed = Math.abs(swipedSlide.dataset.index - this.state.currentSlide) || 1;
+        } else {
+            slidesTraversed = 0;
+        }
 
       return slidesTraversed;
     } else {
