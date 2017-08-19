a923c68dc2335073b4e77ee4c16b94067eac5800
kiran <kiran@neostack.com>
2017-08-19 22:24:10 +0530

    integrated #641

--- src/mixins/helpers.js
+++ src/mixins/helpers.js
@@ -94,10 +94,10 @@ var helpers = {
     });
   },
   getWidth: function getWidth(elem) {
-    return elem.getBoundingClientRect().width || elem.offsetWidth || 0;
+    return elem && (elem.getBoundingClientRect().width || elem.offsetWidth) || 0;
   },
   getHeight(elem) {
-    return elem === null ? 0 : elem.getBoundingClientRect().height || elem.offsetHeight || 0;
+    return elem && (elem.getBoundingClientRect().height || elem.offsetHeight) || 0;
   },
   adaptHeight: function () {
     if (this.props.adaptiveHeight) {
