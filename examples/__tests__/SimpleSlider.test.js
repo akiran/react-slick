650b710a168ec1d1f6a49c70c2289d7ede9bc987
kiran <kiran@neostack.com>
2017-08-20 20:26:32 +0530

    added tests to simple slider

--- examples/__tests__/SimpleSlider.test.js
+++ examples/__tests__/SimpleSlider.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 import SimpleSlider from '../SimpleSlider';
 
-describe('sample enzyme test', function() {
+describe('Simple Slider', function() {
   it('should have 8 slides (6 actual and 2 clone slides)', function() {
     const wrapper = mount(<SimpleSlider />);
     expect(wrapper.find('.slick-slide').length).toEqual(8);
@@ -15,5 +15,21 @@ describe('sample enzyme test', function() {
   it('should have 1 active slide', function() {
     const wrapper = mount(<SimpleSlider />);
     expect(wrapper.find('.slick-slide.slick-active').length).toEqual(1);
+  });
+  it('should have 6 dots', function() {
+    const wrapper = mount(<SimpleSlider />);
+    expect(wrapper.find('.slick-dots').children().length).toEqual(6);
+  });
+  it('should have 1 active dot', function() {
+    const wrapper = mount(<SimpleSlider />);
+    expect(wrapper.find('.slick-dots .slick-active').length).toEqual(1);
+  });
+  it('should have a prev arrow', function () {
+    const wrapper = mount(<SimpleSlider />);
+    expect(wrapper.find('.slick-prev').length).toEqual(1);
+  });
+  it('should have a next arrow', function () {
+    const wrapper = mount(<SimpleSlider />);
+    expect(wrapper.find('.slick-next').length).toEqual(1);
   });
 });
