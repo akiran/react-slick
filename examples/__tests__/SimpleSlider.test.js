2ab0206c461b39dcf98bc296c10099462e529ff4
laveesingh <laveechaudharytheone@gmail.com>
2018-01-24 17:50:37 +0530

    beautified SimpleSlider snapshot tests

--- examples/__tests__/SimpleSlider.test.js
+++ examples/__tests__/SimpleSlider.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import SimpleSlider from '../SimpleSlider';
 import { repeatClicks } from '../../test-helpers';
+import { html as beautify_html } from 'js-beautify'
 
 describe('Simple Slider', function () {
   it('should have 13 slides (1(preclone) + 6(actual) + 6(postclone))', function () {
@@ -73,21 +74,21 @@ describe('Simple Slider', function () {
 describe("Simple Slider Snapshots", function () {
   it("slider initial state", function () {
     const wrapper = mount(<SimpleSlider />);
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(beautify_html(wrapper.html())).toMatchSnapshot()
   });
   it("click on next button", function () {
     const wrapper = mount(<SimpleSlider />);
     wrapper.find('.slick-next').simulate('click')
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(beautify_html(wrapper.html())).toMatchSnapshot()
   });
   it("click on prev button", function () {
     const wrapper = mount(<SimpleSlider />);
     wrapper.find('.slick-prev').simulate('click')
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(beautify_html(wrapper.html())).toMatchSnapshot()
   });
   it("click on 3rd dot", function () {
     const wrapper = mount(<SimpleSlider />);
     wrapper.find('.slick-dots button').at(2).simulate('click')
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(beautify_html(wrapper.html())).toMatchSnapshot()
   })
 });
