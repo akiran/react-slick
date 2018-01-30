07da344d1b8225377a016b8e00c8ecc47bde7ba6
lavee singh <lavees96@gmail.com>
2018-01-30 16:21:04 +0530

    removed VerticalAndFade example

--- docs/demos.jsx
+++ docs/demos.jsx
@@ -25,7 +25,6 @@ import DynamicSlides  from '../examples/DynamicSlides'
 import VerticalMode  from '../examples/VerticalMode'
 import SwipeToSlide from '../examples/SwipeToSlide'
 import VerticalSwipeToSlide from '../examples/VerticalSwipeToSlide'
-import VerticalAndFade from '../examples/VerticalAndFade'
 import CustomPaging from '../examples/CustomPaging'
 import CustomSlides from '../examples/CustomSlides'
 
@@ -57,7 +56,6 @@ export default class App extends React.Component {
         <VerticalMode />
         <SwipeToSlide />
         <VerticalSwipeToSlide />
-        <VerticalAndFade />
       </div>
     );
   }
