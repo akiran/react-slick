4be9edf82ed76c0e297641478be3dc2ae744ee30
laveesingh <lavees96@gmail.com>
2018-03-07 18:25:34 +0530

    canGoNext implemented implemented as a pure function
    replacement of the mixin are in progress

--- src/utils/innerSliderUtils.js
+++ src/utils/innerSliderUtils.js
@@ -76,3 +76,16 @@ export const getSwipeDirection = (touchObject, verticalSwiping=false) => {
   return 'vertical';
 
 }
+
+export const canGoNext = spec => {
+  let canGo = true
+  if (!spec.infinite) {
+    if (spec.centerMode && spec.currentSlide >= spec.slideCount - 1) {
+      canGo = false
+    } else if (spec.slideCount <= spec.slidesToShow ||
+      spec.currentSlide >= spec.slideCount - spec.slidesToShow) {
+      canGo = false
+    }
+  }
+  return canGo
+}
