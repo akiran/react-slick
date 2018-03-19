82e64f81dab7c328871cb8edef94a788d010912f
laveesingh <lavees96@gmail.com>
2018-03-19 14:21:30 +0530

    removed getSlideCount mixin and created a pure function instead

--- src/mixins/event-handlers.js
+++ src/mixins/event-handlers.js
@@ -3,46 +3,9 @@ import {getTrackCSS, getTrackLeft, getTrackAnimateCSS} from './trackHelper';
 import assign from 'object-assign';
 import ReactDOM from 'react-dom';
 import { siblingDirection } from '../utils/trackUtils'
-import { getWidth, getHeight, getSwipeDirection, checkNavigable } from '../utils/innerSliderUtils'
+import { getWidth, getHeight, getSwipeDirection, checkNavigable, getSlideCount } from '../utils/innerSliderUtils'
 
 var EventHandlers = {
-  getSlideCount() {
-    const centerOffset = this.props.centerMode ? this.state.slideWidth * Math.floor(this.props.slidesToShow / 2) : 0;
-
-    if (this.props.swipeToSlide) {
-      let swipedSlide;
-
-      const slickList = ReactDOM.findDOMNode(this.list);
-
-      const slides = slickList.querySelectorAll('.slick-slide');
-
-      Array.from(slides).every((slide) => {
-        if (!this.props.vertical) {
-          if (slide.offsetLeft - centerOffset + (getWidth(slide) / 2) > this.state.swipeLeft * -1) {
-            swipedSlide = slide;
-            return false;
-          }
-        } else {
-          if (slide.offsetTop + (getHeight(slide) / 2) > this.state.swipeLeft * -1) {
-            swipedSlide = slide;
-            return false;
-          }
-        }
-
-        return true;
-      });
-
-      if (!swipedSlide) {
-        return 0;
-      }
-      const currentIndex = this.props.rtl === true ? this.state.slideCount - this.state.currentSlide : this.state.currentSlide; 
-      const slidesTraversed = Math.abs(swipedSlide.dataset.index - currentIndex) || 1;
-
-      return slidesTraversed;
-    } else {
-      return this.props.slidesToScroll;
-    }
-  },
   swipeEnd: function (e) {
     if (!this.state.dragging) {
       if (this.props.swipe) {
@@ -90,14 +53,14 @@ var EventHandlers = {
 
         case 'left':
         case 'up':
-          newSlide = this.state.currentSlide + this.getSlideCount();
+          newSlide = this.state.currentSlide + getSlideCount({...this.props, ...this.state, listRef: this.list});
           slideCount = this.props.swipeToSlide ? checkNavigable({...this.props, ...this.state}, newSlide) : newSlide;
           this.setState({ currentDirection: 0 })
           break;
 
         case 'right':
         case 'down':
-          newSlide = this.state.currentSlide - this.getSlideCount();
+          newSlide = this.state.currentSlide - getSlideCount({...this.props, ...this.state, listRef: this.list});
           slideCount = this.props.swipeToSlide ? checkNavigable({...this.props, ...this.state}, newSlide) : newSlide;
           this.setState({ currentDirection: 1 })
           break;
--- src/utils/innerSliderUtils.js
+++ src/utils/innerSliderUtils.js
@@ -375,3 +375,35 @@ export const checkNavigable = (spec, index) => {
   }
   return index
 }
+export const getSlideCount = spec => {
+    const centerOffset = spec.centerMode ? spec.slideWidth * Math.floor(spec.slidesToShow / 2) : 0
+    if (spec.swipeToSlide) {
+      let swipedSlide
+      const slickList = ReactDOM.findDOMNode(spec.listRef)
+      const slides = slickList.querySelectorAll('.slick-slide')
+      Array.from(slides).every(slide => {
+        if (!spec.vertical) {
+          if (slide.offsetLeft - centerOffset + (getWidth(slide) / 2) > spec.swipeLeft * -1) {
+            swipedSlide = slide
+            return false
+          }
+        } else {
+          if (slide.offsetTop + (getHeight(slide) / 2) > spec.swipeLeft * -1) {
+            swipedSlide = slide
+            return false
+          }
+        }
+
+        return true
+      })
+
+      if (!swipedSlide) {
+        return 0
+      }
+      const currentIndex = spec.rtl === true ? spec.slideCount - spec.currentSlide : spec.currentSlide;
+      const slidesTraversed = Math.abs(swipedSlide.dataset.index - currentIndex) || 1
+      return slidesTraversed
+    } else {
+      return spec.slidesToScroll
+    }
+}
