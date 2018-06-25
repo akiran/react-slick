01396795724277d978bf938ac2c8dc1eb322d355
Mikhail Nazarov <mcfoton@gmail.com>
2018-06-25 10:40:37 +0300

    fix rtl (finite) example

--- src/utils/innerSliderUtils.js
+++ src/utils/innerSliderUtils.js
@@ -48,7 +48,7 @@ export const getSwipeDirection = (touchObject, verticalSwiping = false) => {
   xDist = touchObject.startX - touchObject.curX;
   yDist = touchObject.startY - touchObject.curY;
   r = Math.atan2(yDist, xDist);
-  swipeAngle = Math.round(r * 180 / Math.PI);
+  swipeAngle = Math.round((r * 180) / Math.PI);
   if (swipeAngle < 0) {
     swipeAngle = 360 - Math.abs(swipeAngle);
   }
@@ -76,11 +76,11 @@ export const getSwipeDirection = (touchObject, verticalSwiping = false) => {
 export const canGoNext = spec => {
   let canGo = true;
   if (!spec.infinite) {
-    if (spec.centerMode && spec.currentSlide >= spec.slideCount - 1) {
-      canGo = false;
-    } else if (
+    if (
+      (spec.centerMode && spec.currentSlide >= spec.slideCount - 1) ||
       spec.slideCount <= spec.slidesToShow ||
-      spec.currentSlide >= spec.slideCount - spec.slidesToShow
+      (!spec.rtl && spec.currentSlide >= spec.slideCount - spec.slidesToShow) ||
+      (spec.rtl && spec.currentSlide + 1 >= spec.slideCount)
     ) {
       canGo = false;
     }
@@ -195,7 +195,7 @@ export const slideHandler = spec => {
       finalSlide = animationSlide + slideCount;
       if (!infinite) finalSlide = 0;
       else if (slideCount % slidesToScroll !== 0)
-        finalSlide = slideCount - slideCount % slidesToScroll;
+        finalSlide = slideCount - (slideCount % slidesToScroll);
     } else if (!canGoNext(spec) && animationSlide > currentSlide) {
       animationSlide = finalSlide = currentSlide;
     } else if (centerMode && animationSlide >= slideCount) {
@@ -265,7 +265,8 @@ export const changeSlide = (spec, options) => {
     slideOffset = indexOffset === 0 ? slidesToScroll : indexOffset;
     targetSlide = currentSlide + slideOffset;
     if (lazyLoad && !infinite) {
-      targetSlide = (currentSlide + slidesToScroll) % slideCount + indexOffset;
+      targetSlide =
+        ((currentSlide + slidesToScroll) % slideCount) + indexOffset;
     }
   } else if (options.message === "dots") {
     // Click on dots
@@ -704,7 +705,7 @@ export const getTrackLeft = spec => {
       slideCount % slidesToScroll !== 0 &&
       slideIndex + slidesToScroll > slideCount
     ) {
-      slidesToOffset = slidesToShow - slideCount % slidesToScroll;
+      slidesToOffset = slidesToShow - (slideCount % slidesToScroll);
     }
     if (centerMode) {
       slidesToOffset = parseInt(slidesToShow / 2);
@@ -714,7 +715,12 @@ export const getTrackLeft = spec => {
   verticalOffset = slidesToOffset * slideHeight;
 
   if (!vertical) {
-    targetLeft = slideIndex * slideWidth * -1 + slideOffset;
+    if (!spec.rtl) {
+      targetLeft = slideIndex * slideWidth * -1 + slideOffset;
+    } else {
+      targetLeft =
+        (slideIndex - slidesToShow + 1) * slideWidth * -1 + slideOffset;
+    }
   } else {
     targetLeft = slideIndex * slideHeight * -1 + verticalOffset;
   }
