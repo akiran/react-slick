a13c24f630f0d3072c5c0bfcc736fe3c73c6fcb6
HelKyle <helkylefighting@gmail.com>
2018-07-11 22:25:43 +0800

    Remove unnecessary div in single mode.
    Remove unnecessary div in single mode.

--- src/slider.js
+++ src/slider.js
@@ -160,44 +160,49 @@ export default class Slider extends React.Component {
     }
     let newChildren = [];
     let currentWidth = null;
-    for (
-      let i = 0;
-      i < children.length;
-      i += settings.rows * settings.slidesPerRow
-    ) {
-      let newSlide = [];
+
+    if (settings.rows * settings.slidesPerRow === 1) {
+      newChildren = children;
+    } else {
       for (
-        let j = i;
-        j < i + settings.rows * settings.slidesPerRow;
-        j += settings.slidesPerRow
+        let i = 0;
+        i < children.length;
+        i += settings.rows * settings.slidesPerRow
       ) {
-        let row = [];
-        for (let k = j; k < j + settings.slidesPerRow; k += 1) {
-          if (settings.variableWidth && children[k].props.style) {
-            currentWidth = children[k].props.style.width;
+        let newSlide = [];
+        for (
+          let j = i;
+          j < i + settings.rows * settings.slidesPerRow;
+          j += settings.slidesPerRow
+        ) {
+          let row = [];
+          for (let k = j; k < j + settings.slidesPerRow; k += 1) {
+            if (settings.variableWidth && children[k].props.style) {
+              currentWidth = children[k].props.style.width;
+            }
+            if (k >= children.length) break;
+            row.push(
+              React.cloneElement(children[k], {
+                key: 100 * i + 10 * j + k,
+                tabIndex: -1,
+                style: {
+                  width: `${100 / settings.slidesPerRow}%`,
+                  display: "inline-block"
+                }
+              })
+            );
           }
-          if (k >= children.length) break;
-          row.push(
-            React.cloneElement(children[k], {
-              key: 100 * i + 10 * j + k,
-              tabIndex: -1,
-              style: {
-                width: `${100 / settings.slidesPerRow}%`,
-                display: "inline-block"
-              }
-            })
-          );
+          newSlide.push(<div key={10 * i + j}>{row}</div>);
         }
-        newSlide.push(<div key={10 * i + j}>{row}</div>);
-      }
-      if (settings.variableWidth) {
-        newChildren.push(
-          <div key={i} style={{ width: currentWidth }}>
-            {newSlide}
-          </div>
-        );
-      } else {
-        newChildren.push(<div key={i}>{newSlide}</div>);
+        if (settings.variableWidth) {
+          newChildren.push(
+            <div key={i} style={{ width: currentWidth }}>
+              {newSlide}
+            </div>
+          );
+        } else {
+          newChildren.push(<div key={i}>{newSlide}</div>);
+        }
       }
     }
 
