8c7eac37e4563e7fb9602c0340f8be1df9c82aa7
Emad-salah <heroinwars@gmail.com>
2018-09-25 16:51:46 +0100

    Click events are now working on active slides

--- src/track.js
+++ src/track.js
@@ -58,6 +58,7 @@ var getSlideStyle = function(spec) {
       style.left = -spec.index * parseInt(spec.slideWidth);
     }
     style.opacity = spec.currentSlide === spec.index ? 1 : 0;
+    style.visibility = spec.currentSlide === spec.index ? 'visible' : 'hidden';
     style.transition =
       "opacity " +
       spec.speed +
