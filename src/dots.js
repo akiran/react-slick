fa9cf349f3b5e88e194b7f8ec78009bd10545d87
Melanie DUVERGER <melanie.duverger@viacesi.fr>
2019-04-30 14:41:55 +0200

    :bug: Dots: fix extra space that mismatch with slide ids

--- src/dots.js
+++ src/dots.js
@@ -27,9 +27,9 @@ var getNthDotAssociatedSlides = function(n, spec) {
   ) {
     if (i >= spec.slideCount && spec.infinite) {
       var newIndex = i - spec.slideCount;
-      nthDotAssociatedSlides += ` ${spec.name}-${newIndex}`;
+      nthDotAssociatedSlides += `${spec.name}-${newIndex}`;
     } else {
-      nthDotAssociatedSlides += ` ${spec.name}-${i}`;
+      nthDotAssociatedSlides += `${spec.name}-${i}`;
     }
   }
 
