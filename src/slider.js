cde2704f09d89a496f53a1a7a5e2b7193fe0280b
pankaj kumar <kumar.pankaj2204@gmail.com>
2020-05-10 21:22:34 +0530

    default children return in case of unslick

--- src/slider.js
+++ src/slider.js
@@ -197,12 +197,16 @@ export default class Slider extends React.Component {
 
     if (settings === "unslick") {
       const className = "regular slider " + (this.props.className || "");
-      return <div className={className}>{newChildren}</div>;
+      return <div className={className}>{children}</div>;
     } else if (newChildren.length <= settings.slidesToShow) {
       settings.unslick = true;
     }
     return (
-      <InnerSlider style={this.props.style} ref={this.innerSliderRefHandler} {...settings}>
+      <InnerSlider
+        style={this.props.style}
+        ref={this.innerSliderRefHandler}
+        {...settings}
+      >
         {newChildren}
       </InnerSlider>
     );
