8860dc4f327ee312da856ea5cf7c8671705850a7
kiran <kiran@neostack.com>
2020-05-12 00:52:33 +0530

    rename jsx to js files

---
+++ docs/routes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var React = require("react");
+var Router = require("react-router");
+var Route = Router.Route;
+var Docs = require("./docs");
+
+var path =
+  process.env.NODE_ENV === "dev_docs" ? "/" : "/opensource/react-slick";
+var routes = <Route name="app" path={path} handler={Docs}></Route>;
+
+module.exports = routes;
