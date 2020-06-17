03e0bd8d029004d555383be17fc3cd74b32420ce
Amumu <yoyo837@hotmail.com>
2020-06-17 20:27:22 +0800

    Fix autoplay when update (#20)
    * prettier
    
    * perttier
    
    * fix autoplay when update
    
    * update

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -125,6 +125,7 @@ export class InnerSlider extends React.Component {
     this.ro.disconnect();
   };
   UNSAFE_componentWillReceiveProps = nextProps => {
+    const { autoplay, autoplaySpeed } = this.props;
     let spec = {
       listRef: this.list,
       trackRef: this.track,
@@ -156,6 +157,12 @@ export class InnerSlider extends React.Component {
             React.Children.count(nextProps.children) - nextProps.slidesToShow,
           currentSlide: this.state.currentSlide
         });
+      }
+      if (
+        autoplay === nextProps.autoplay &&
+        autoplaySpeed === nextProps.autoplaySpeed
+      ) {
+        return;
       }
       if (nextProps.autoplay) {
         this.autoPlay("update");
