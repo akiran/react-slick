3d73c7b388dc9307fea52f55ce3e8ff110e15dd3
Vadim Sviridov <avangardsv@gmail.com>
2021-01-15 18:35:47 +0200

    fix change slider for infinity case

--- src/utils/innerSliderUtils.js
+++ src/utils/innerSliderUtils.js
@@ -274,9 +274,9 @@ export const changeSlide = (spec, options) => {
       previousInt = currentSlide - slideOffset;
       targetSlide = previousInt === -1 ? slideCount - 1 : previousInt;
     }
-    if (!infinite) {
-      targetSlide = previousTargetSlide - slidesToScroll;
-    }
+    // if (!infinite) {
+    //   targetSlide = previousTargetSlide - slidesToScroll;
+    // }
   } else if (options.message === "next") {
     slideOffset = indexOffset === 0 ? slidesToScroll : indexOffset;
     targetSlide = currentSlide + slideOffset;
@@ -284,9 +284,9 @@ export const changeSlide = (spec, options) => {
       targetSlide =
         ((currentSlide + slidesToScroll) % slideCount) + indexOffset;
     }
-    if (!infinite) {
-      targetSlide = previousTargetSlide + slidesToScroll;
-    }
+    // if (!infinite) {
+    //   targetSlide = previousTargetSlide + slidesToScroll;
+    // }
   } else if (options.message === "dots") {
     // Click on dots
     targetSlide = options.index * options.slidesToScroll;
