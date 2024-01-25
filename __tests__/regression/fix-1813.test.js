358d8c04740d20891e1b0bdae6a47d5022af4dc6
akiran <kiran@neostack.com>
2024-01-25 14:55:36 +0530

    created a generic slider component for tests

--- __tests__/regression/fix-1813.test.js
+++ __tests__/regression/fix-1813.test.js
@@ -20,6 +20,7 @@ import {
   getSlidesCount,
   hasClass
 } from "../../test-utils";
+import { GenericSliderComponent } from "../TestComponents";
 
 function MultipleItems() {
   const settings = {
@@ -29,40 +30,7 @@ function MultipleItems() {
     slidesToShow: 9,
     slidesToScroll: 3
   };
-  return (
-    <div>
-      <h2> Multiple items </h2>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
-      </Slider>
-    </div>
-  );
+  return <GenericSliderComponent slidesCount={9} settings={settings} />;
 }
 
 describe("Multiple Items with slidesToShow = slides count in infinite mode", function() {
