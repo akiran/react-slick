372349a3ed21b9b5540b6fcc2e81b31e461e202d
akiran <kiran@neostack.com>
2024-02-09 21:50:38 +0530

    updated jest config to run tests only with .test.js extension

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/test-setup.js"]
+  setupFilesAfterEnv: ["<rootDir>/test-setup.js"],
+  // testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ]
+  testMatch: ["**/?(*.)+(test).[jt]s?(x)"]
 };
