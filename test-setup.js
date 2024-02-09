cf3130744e4e2ad5870d9286ffd95ecf0152fa02
akiran <kiran@neostack.com>
2024-02-09 21:49:31 +0530

    Added regenerator-runtime for async jest tests

--- test-setup.js
+++ test-setup.js
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
+import "regenerator-runtime/runtime";
 
 //Fix for "matchMedia not present, legacy browsers require a polyfill jest" error
 window.matchMedia =
