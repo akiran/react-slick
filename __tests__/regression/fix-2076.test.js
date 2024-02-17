9227178fa3bfb3b61134cd1e512c10dee7a89c81
akiran <kiran@neostack.com>
2024-02-17 13:32:25 +0530

    remove empty test file

--- __tests__/regression/fix-2076.test.js
+++
@@ -1,3 +0,0 @@
-// Test fix of #2076: Click on any slide in fade mode is triggering the click on last slide.
-
-// Todo: Need to write the test in playwright (Couldn't trigger click on inner slide in fade mode with testing library)
