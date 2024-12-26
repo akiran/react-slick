901840e3ed973be8b5235f8ce7cc5d5191bbeb26
akiran <kiran@neostack.com>
2024-12-26 23:59:38 +0530

    unslick if slides <= slidesToShow

--- playwright-tests/regression/fix-1930/fix-1930.story.tsx
+++ playwright-tests/regression/fix-1930/fix-1930.story.tsx
@@ -1,10 +1,29 @@
 import Slider from "../../../src-jsx";
 import React from "react";
 
-export default function VerticalMode() {
+export function VerticalModeFinite() {
   const settings = {
     dots: true,
     infinite: false,
+    vertical: true,
+    slidesToShow: 3
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <div style={{ height: 100 }}>1</div>
+      </div>
+      <div>
+        <div style={{ height: 100 }}>2</div>
+      </div>
+    </Slider>
+  );
+}
+
+export function VerticalModeInfinite() {
+  const settings = {
+    dots: true,
+    infinite: true,
     vertical: true,
     slidesToShow: 3
   };
